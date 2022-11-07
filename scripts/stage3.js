a2e3ca4c6578f6814831999cd0482eeeafe4f987
JaviAres <jam061097@gmail.com>
2022-11-07 21:50:17 +0100

    Commit message issues

--- scripts/stage3.js
+++ scripts/stage3.js
@@ -2,4 +2,7 @@ function transicionfinal(){
     final.removeChild(final.firstElementChild);
     document.body.style.backgroundImage="url('../images/gridania.png')";
     document.getElementById("boton-final").style.display='none';
+    document.getElementById("yo").style.display='block';
+    document.getElementById("fran").style.display='block';
+    document.getElementById("mensaje_bonito").style.display='block';
 }
