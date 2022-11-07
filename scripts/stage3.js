52b4db572e655fbfcb841da55a8bad3d64c9fbef
JaviAres <jam061097@gmail.com>
2022-11-07 21:49:59 +0100

    stage 3 and finishing details

---
+++ scripts/stage3.js
@@ -0,0 +1,5 @@
+function transicionfinal(){
+    final.removeChild(final.firstElementChild);
+    document.body.style.backgroundImage="url('../images/gridania.png')";
+    document.getElementById("boton-final").style.display='none';
+}
